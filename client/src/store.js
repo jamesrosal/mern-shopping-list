4039a6e9bd5df6ef9029d6762475e90e65bf0e9f
James Rosal <jamesfrosal@gmail.com>
2019-07-12 11:54:01 -0500

    message

---
+++ client/src/store.js
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// const initialState = {};
+
+const middleware = [thunk];
+
+/*
+const store = createStore(
+    rootReducer, 
+    initialState, 
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+*/
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+  applyMiddleware(...middleware)
+));
+
+export default store;
+
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://taajames:TAAagency!2000@mern-shopping-faa3m.mongodb.net/test?retryWrites=true&w=majority'
+}
